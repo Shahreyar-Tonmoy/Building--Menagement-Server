248fbb6e4ec8c6888bb3088471ab7bd35d0fcd3c
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-11-27 00:13:12 +0600

    git fix

--- index.js
+++ index.js
@@ -282,7 +282,7 @@ async function run() {
         
 
         // // update opction
-        // // update opction
+        
 
         app.put('/rents/:id' , async (req, res) => {
             const id = req.params.id
